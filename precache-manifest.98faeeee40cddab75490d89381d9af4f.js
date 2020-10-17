748ca6fbaf5c7a1692ea3a6e53d1cd8763d47551
Alice Ma <alicema@berkeley.edu>
2020-10-17 19:30:39 -0400

    Updates

---
+++ precache-manifest.98faeeee40cddab75490d89381d9af4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1912c08768ad16872fe7383ac7eca0e1",
+    "url": "/alice-fm/index.html"
+  },
+  {
+    "revision": "cef04dbf7a45f1da371d",
+    "url": "/alice-fm/static/css/main.4ecc19ae.chunk.css"
+  },
+  {
+    "revision": "684f9ba41a21d5f9fd29",
+    "url": "/alice-fm/static/js/2.c1f31237.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/alice-fm/static/js/2.c1f31237.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cef04dbf7a45f1da371d",
+    "url": "/alice-fm/static/js/main.49296590.chunk.js"
+  },
+  {
+    "revision": "577a47b8487a2a1410e9",
+    "url": "/alice-fm/static/js/runtime-main.c8e796b8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/alice-fm/static/media/logo.5d5d9eef.svg"
+  }
+]);
